9d943f65fbc613f99aa50bfec0aca62e0aa7438d
Feras El-Sharif <49439225+ferasbbm@users.noreply.github.com>
2023-02-26 21:37:26 +0200

    update

--- src/Transcription.php
+++ src/Transcription.php
@@ -4,14 +4,54 @@ namespace Ferasbbm\Transcription;
 
 class Transcription
 {
+    private array $lines;
+
     /**
      * @param $path
      *
+     * @return self
+     * @author <ferasbbm>
+     */
+    public static function load($path): self
+    {
+        $instance = new static();
+        $instance->lines = $instance->discardIrrelevantLines(file($path));
+
+        return $instance;
+    }
+
+    /**
+     * @param array $lines
+     *
+     * @return array
+     * @author <ferasbbm>
+     */
+    private function discardIrrelevantLines(array $lines): array
+    {
+
+        return array_values(
+            array_filter(
+                array_map('trim', $lines),
+                fn ($line) => $line !== 'WEBVTT' && !is_numeric($line) && $line !== ''
+            )
+        );
+    }
+
+    /**
+     * @return array
+     * @author <ferasbbm>
+     */
+    public function lines(): array
+    {
+        return $this->lines;
+    }
+
+    /**
      * @return string
      * @author <ferasbbm>
      */
-    public static function load($path): string
+    public function __toString(): string
     {
-        return file_get_contents($path);
+        return implode('', $this->lines);
     }
 }
--- tests/TranscriptionTest.php
+++ tests/TranscriptionTest.php
@@ -11,8 +11,37 @@ class TranscriptionTest extends TestCase
      * @return void
      * @author <ferasbbm>
      */
-    function it_load_a_vtt_file(): void
+    function it_load_a_vtt_file_as_a_string(): void
     {
-        $this->assertStringEqualsFile(__DIR__ . '/stubs/basic-example.vtt', Transcription::load(__DIR__ . '/stubs/basic-example.vtt'));
+        $file = __DIR__ . '/stubs/basic-example.vtt';
+        $transcription = Transcription::load($file);
+
+        $this->assertStringContainsString('Here is a', $transcription);
+    }
+
+    /**
+     * @test
+     * @return void
+     * @author <ferasbbm>
+     */
+    function it_can_convert_to_an_array_of_lines(): void
+    {
+        $file = __DIR__ . '/stubs/basic-example.vtt';
+
+        $this->assertCount(4, Transcription::load($file)->lines());
+    }
+
+    /**
+     * @test
+     * @return void
+     * @author <ferasbbm>
+     */
+    function it_discards_irrelevant_lines(): void
+    {
+        $file = __DIR__ . '/stubs/basic-example.vtt';
+        $transcription = Transcription::load($file);
+
+        $this->assertStringNotContainsString('WEBVTT', $transcription);
+        $this->assertCount(4, $transcription->lines());
     }
 }
